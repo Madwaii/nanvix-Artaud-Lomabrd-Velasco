1237198ce0d3f5f0be4522e40cd8eb270965b50e
Adrien Artaud <adrienartaud1@orange.fr>
2020-02-12 11:57:33 +0100

    bug corrigés

--- src/kernel/sys/sem.c
+++ src/kernel/sys/sem.c
@@ -25,99 +25,94 @@ unsigned int curkey=1;
 
 SEM* create (int n){
 
-	int i=0;
-	while(tabSem[i].key!=0){
-		i++;
-	}
-	if(i<64){
-		tabSem[i].key=curkey;
-		tabSem[i].counter=n;
-		curkey++;
-		return &tabSem[i];
-	}
-	else{
-		return NULL;
-	}
+    int i=0;
+    while(tabSem[i].key!=0){
+        i++;
+    }
+    if(i<64){
+        tabSem[i].key=curkey;
+        tabSem[i].counter=n;
+        curkey++;
+        return &tabSem[i];
+    }
+    else{
+        return NULL;
+    }
 
 
 }
 
 void down(SEM *sem){
-  if(sem->counter > 0){
-    sem->counter--;
-  }
-  else{
-    sleep(sem->tabWait,5);
-  }
+    if(sem->counter > 0){
+        sem->counter--;
+    }
+    else{
+        sleep(sem->tabWait,5);
+    }
 }
 
 void up(SEM *sem){
-  if(sem->counter==0){
-    if(sem->tabWait[0]!=NULL){
-      wakeup(sem->tabWait);
+    if(sem->counter==0){
+        if(sem->tabWait[0]!=NULL){
+            wakeup(sem->tabWait);
+        }
+        else{
+            sem->counter++;
+        }
     }
-    else{
-      sem->counter++;
-    }
-  }
 
 }
 
 void destroy(SEM *sem){
-  for(int i=0;i<64;i++){
-    sem->tabWait[i]=NULL;
-  }
-	sem->key=0;
+    for(int i=0;i<64;i++){
+        sem->tabWait[i]=NULL;
+    }
+    sem->key=0;
 }
 
 int sys_semget(unsigned key){
-  int i=0;
-  while(i<64){
-    if(tabSem[i].key==key){
-    	return i;
+    int i=0;
+    while(i<64){
+        if(tabSem[i].key==key){
+            return i;
+        }
+        i++;
     }
-		i++;
-  }
-	if(i>=64){
-		SEM* sem= create(1);
-		sem->key=key;
-		int ID=0;
-		while(tabSem[ID].key!=key){
-			ID++;
-		}
-		return ID;
-	}
-	else{
-		return -1;
-	}
+    SEM* sem= create(1);
+    sem->key=key;
+    int ID=0;
+    while(tabSem[ID].key!=key){
+        ID++;
+    }
+    return ID;
 }
 
 int sys_semctl(int semid, int cmd, int val){
-	switch(cmd){
-		case GETVAL:
-			return tabSem[semid].counter;
-		case SETVAL:
-			tabSem[semid].counter=val;
-			return 0;
-		case IPC_RMID:
-			destroy(&tabSem[semid]);
-			return 0;
-		default:
-			break;
-	}
-  return -1;
+    switch(cmd){
+        case GETVAL:
+        return tabSem[semid].counter;
+        case SETVAL:
+        tabSem[semid].counter=val;
+        return 0;
+        case IPC_RMID:
+        destroy(&tabSem[semid]);
+        return 0;
+        default:
+        break;
+    }
+    return -1;
 }
 
 int sys_semop(int semid, int op){
-	if(op>0){
-		up(&tabSem[semid]);
-		return 0;
-	}
-	if(op<0){
-		down(&tabSem[semid]);
-		return 0;
-	}
-	else{
-		return -1;
-	}
+    if(op>0){
+        up(&tabSem[semid]);
+        return 0;
+    }
+    if(op<0){
+        down(&tabSem[semid]);
+        return 0;
+    }
+    else{
+        return -1;
+    }
 }
